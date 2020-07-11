db0b1ca71296bd856c7ba45d52d8270eb6932a4b
pcraven18 <55059815+pcraven18@users.noreply.github.com>
2020-07-11 16:13:49 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { CompanyComponent } from './company/company.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    CompanyComponent
   ],
   imports: [
     BrowserModule
